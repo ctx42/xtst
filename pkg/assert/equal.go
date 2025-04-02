41d3c0474fa2336174f9763d5df8b2f7b2477b52
Rafal Zajac <rzajac@gmail.com>
2025-04-02 13:15:28 +0200

    Add assert.Equal.

---
+++ pkg/assert/equal.go
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: (c) 2025 Rafal Zajac <rzajac@gmail.com>
+// SPDX-License-Identifier: MIT
+
+package assert
+
+import (
+	"github.com/ctx42/xtst/pkg/check"
+	"github.com/ctx42/xtst/pkg/tester"
+)
+
+// Equal asserts both values are equal. Returns true if they are, otherwise
+// marks the test as failed, writes error message to test log and returns false.
+func Equal(t tester.T, want, have any, opts ...check.Option) bool {
+	t.Helper()
+	if err := check.Equal(want, have, opts...); err != nil {
+		t.Error(err)
+		return false
+	}
+	return true
+}
---
+++ pkg/assert/equal_test.go
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: (c) 2025 Rafal Zajac <rzajac@gmail.com>
+// SPDX-License-Identifier: MIT
+
+package assert
+
+import (
+	"testing"
+
+	"github.com/ctx42/xtst/internal/affirm"
+	"github.com/ctx42/xtst/pkg/check"
+	"github.com/ctx42/xtst/pkg/tester"
+)
+
+func Test_Equal(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		// --- Given ---
+		tspy := tester.New(t).Close()
+
+		// --- When ---
+		have := Equal(tspy, 42, 42)
+
+		// --- Then ---
+		affirm.True(t, have)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		// --- Given ---
+		tspy := tester.New(t)
+		tspy.ExpectError()
+		tspy.IgnoreLogs()
+		tspy.Close()
+
+		// --- When ---
+		have := Equal(tspy, 42, 44)
+
+		// --- Then ---
+		affirm.False(t, have)
+	})
+
+	t.Run("log message with trail", func(t *testing.T) {
+		// --- Given ---
+		tspy := tester.New(t)
+		tspy.ExpectError()
+		tspy.ExpectLogContain("  trail: type.field\n")
+		tspy.Close()
+
+		opt := check.WithTrail("type.field")
+
+		// --- When ---
+		have := Equal(tspy, 42, 44, opt)
+
+		// --- Then ---
+		affirm.False(t, have)
+	})
+}
